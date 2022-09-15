a929a67e63569f68f193d836cd52e96a181fc223
Kevin Alce <Kevinalce33@gmail.com>
2022-09-15 09:10:41 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
+
+  const taskUL = document.querySelector('#tasks')
+  //const deleteIt = document.getElementById('li-it')
+
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.querySelector('#new-task').value)
+    form.reset();
+  })
+
+  function buildToDo (toDo) {
+    let li = document.createElement('li');
+    li.id = 'li-it'
+    taskUL.append(li);
+    li.textContent = `${toDo}`;
+    let btn = document.createElement('button');
+    btn.id = 'click'
+    btn.textContent = 'x'
+    li.append(btn)
+    btn.addEventListener('click', () => {
+      btn.parentNode.remove();
+    })
+    
+
+  }
+
+
+  
+}
+ 
+  
   // your code here
-});
+)
+
+
